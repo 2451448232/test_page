89ee7444a24aa47292c57ea2dfc50fc16ab71aea
slhtyosoro <2118821931@qq.com>
2023-08-27 18:12:16 +0800

    first

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-theme-amaranth",
+      options: {},
+    },
+  ],
+  // siteMetadata: {
+  //   title: "Gatsby Theme Amaranth",
+  //   description: "A Gatsby theme for Amaranth",
+  //   keywords: ["gatsby", "theme", "amaranth"],
+  //   siteUrl: "https://gatsby-theme-amaranth.netlify.com",
+  // }
+};
